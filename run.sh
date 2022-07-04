f238c42b81a4fd96999bfcde50e915f8f2d6bccf
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2022-07-04 23:22:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,14 +15,51 @@ sudo git clone https://github.com/skmudassir-it/House_price_prediction.git
 cd House_price_prediction
 sudo python3 -m venv project
 source project/bin/activate
-sudo pip install wheel
-sudo pip install gunicorn flask
-sudo pip install -r requirements.txt
-sudo pip install uwsgi flask
+pip3 install wheel
+sudo python3 -m pip install gunicorn flask
+sudo python3 -m pip install uwsgi flask
+pip3 install -r requirements.txt
+pip3 install uwsgi flask
 sudo mv main.py ./project/app.py
 sudo mv static ./project/
 sudo mv templates ./project/
 sudo mv model ./project/
+#python3 app.py
+sudo nano project.ini
+
+'''
+[uwsgi]
+module = app:app
+master = true
+processes = 5
+socket = project.sock
+chmod-socket = 660
+vacuum = true
+die-on-term = true
+logto = /home/ubuntu/House_price_prediction/project/project.log
+'''
+
+sudo nano /etc/systemd/system/project.service
+
+'''
+[Unit]
+Description=uWSGI instance to serve project
+After=network.target
+
+[Service]
+User=root
+Group=www-data
+WorkingDirectory=/home/ubuntu/House_price_prediction
+Environment="PATH=/home/ubuntu/House_price_prediction/project/bin"
+ExecStart=/home/ubuntu/House_price_prediction/project/bin/uwsgi --ini project.ini
+
+[Install]
+WantedBy=multi-user.target
+'''
+
+sudo systemctl start project
+sudo systemctl enable project
+sudo systemctl status project
 
 
 cd /var/www/html
@@ -37,23 +74,9 @@ cd /var/www/html/House_price_prediction
 
 sudo nano /etc/systemd/system/project.service
 
-[Unit]
-Description=Gunicorn instance to serve One
-After=network.target
 
-[Service]
-User=root
-Group=www-data
-WorkingDirectory=/var/www/html/House_price_prediction
-Environment="PATH=/var/www/html/House_price_prediction/project/bin"
-ExecStart=var/www/html/House_price_prediction/project/bin/gunicorn --workers 3 --bind unix:project.sock -m 007 app:app
 
-[Install]
-WantedBy=multi-user.target
 
-sudo systemctl start project
-sudo systemctl enable project
-sudo systemctl status project
 
 #python3 main.py http://127.0.0.1:5000
 
